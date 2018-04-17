d8edf82e3abe7b5ae82307f5766cff98633c3e3e
Joyce Chun <wooyangee@gmail.com>
2018-04-17 06:00:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
-number = 4
+
 def countdown
-  while number >= 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-  end
+  number = 4
+    while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    end
 end 
